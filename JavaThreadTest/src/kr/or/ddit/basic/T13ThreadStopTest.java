65bb628f5272728c20db8aaad45d6a6d7f8387ac
Jangyeji <crush03203@naver.com>
2022-09-30 17:02:52 +0900

    2022.09.30

--- JavaThreadTest/src/kr/or/ddit/basic/T13ThreadStopTest.java
+++ JavaThreadTest/src/kr/or/ddit/basic/T13ThreadStopTest.java
@@ -6,16 +6,26 @@ public class T13ThreadStopTest {
 	 * */
 	public static void main(String[] args) {
 
-		ThreadStopEx1 th1 = new ThreadStopEx1();
-		th1.start();
+//		ThreadStopEx1 th1 = new ThreadStopEx1();
+//		th1.start();
+//
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException ex) {
+//			ex.printStackTrace();
+//		}
+//		// th1.stop();
+//		th1.setStop(true);
+
+		ThreadStopEx2 th2 = new ThreadStopEx2();
+		th2.start();
 
 		try {
 			Thread.sleep(1000);
-		} catch (InterruptedException ex) {
-			ex.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		//th1.stop();
-		th1.setStop(true);
+		th2.interrupt();
 	}
 
 }
@@ -36,5 +46,45 @@ class ThreadStopEx1 extends Thread {
 		System.out.println("자원 정리중...");
 		System.out.println("실행종료...");
 
+	}
+}
+
+/*
+ * interrupt() 메서드를 이용하여 스레드를 멈추게 하는 방법
+ */
+class ThreadStopEx2 extends Thread {
+
+	@Override
+	public void run() {
+//		// 방법 1 => sleep()메서드나 join()메서드 등을 사용했을 때
+//		// interrupt()메서드를 호출하면 InterruptedException
+//		// 이 발생한다
+//
+//		try {
+//			while (true) {
+//				System.out.println("스레드 처리 중...");
+//				Thread.sleep(1);
+//			}
+//		} catch (InterruptedException ex) {
+//		}
+
+		// 방법 2 => 인터럽트가 걸렸는지 검사하는 방법
+
+		while (true) {
+			System.out.println("스레드 처리중...");
+
+			// 검사방법 1 => 스레드의 인스턴스객체용 메서드를 이용하는 방법
+//			if (this.isInterrupted()) {
+//				System.out.println("인스턴스용 isInterrupted()");
+//				break;
+//			}
+			// 검사방법2 => 스레드의 정적메서드를 이용하는 방법
+			if (Thread.interrupted()) {
+				System.out.println("정적 메서드 interrupted()");
+				break;
+			}
+		}
+		System.out.println("자원 정리 중...");
+		System.out.println("실행종료...");
 	}
 }
---
+++ JavaThreadTest/src/kr/or/ddit/basic/T14ThreadShareDataTest.java
@@ -0,0 +1,90 @@
+package kr.or.ddit.basic;
+
+/*
+ * 원주율을 계산하는 스레드가 있고, 계산된 원주율을 출력하는 스레드가 있다.
+ * 원주율을 계산한 후 이 값을 출력하는 프로그램을 작성하시오.
+ * (이때 원주율을 저장하는 객체가 필요하다.)
+ * */
+public class T14ThreadShareDataTest {
+
+}
+
+class ShareData {
+	private double result; // 원주율이 저장될 변수
+	private boolean isOk = false; // 원주율 계산 완료여부 확인용.
+
+	public double getResult() {
+		return result;
+	}
+
+	public void setResult(double result) {
+		this.result = result;
+	}
+
+	public boolean isOk() {
+		return isOk;
+	}
+
+	public void setOk(boolean isOk) {
+		this.isOk = isOk;
+	}
+
+}
+
+//원주율 계산을 위한 스레드 클래스
+class CalcPIThread extends Thread {
+
+	private ShareData sd;
+
+	public CalcPIThread(ShareData sd) {
+		this.sd = sd;
+	}
+
+	@Override
+	public void run() {
+		/*
+		 * 원주율 = (1/1 - 1/3 + /5 - 1/7 + 1/9 .....) * 4;
+		 * 			1  -  3  +  5 -  7  +  9 => 분모
+		 * 			0	  1     2    3     4 => 분모를 2로 나눈 몫
+		 * */
+
+		double sum = 0.0;
+		for (int i = 1; i < 15000000; i += 2) {
+			if (((i / 2) % 2) == 0) {
+
+			} else {
+				sum -= (1.0 / i);
+			}
+		}
+		sd.setResult(sum * 4);
+		sd.setOk(true); // 원주율 계산이 완료되었음을 나타냄
+	}
+
+}
+
+//계산된 원주율을 출력하는 스레드
+class PrintPIThread extends Thread {
+
+	private ShareData sd;
+
+	public PrintPIThread(ShareData sa) {
+		this.sd = sd;
+
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			// 원주율 계산이 완료 될때까지 기다린다.
+			if (sd.isOk()) {
+				break;
+			}
+		}
+
+		System.out.println();
+		System.out.println("계산된 원주율 : " + sd.getResult());
+		System.out.println("       PI : " + Math.PI);
+
+	}
+
+}
