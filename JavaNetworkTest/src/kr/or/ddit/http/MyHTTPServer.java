0bf02f685ff75eb0152a4d9e5262f947e2056a99
Jangyeji <crush03203@naver.com>
2022-10-24 17:39:53 +0900

    2022.10.24 High

--- JavaNetworkTest/src/kr/or/ddit/http/MyHTTPServer.java
+++ JavaNetworkTest/src/kr/or/ddit/http/MyHTTPServer.java
@@ -1,20 +1,22 @@
 package kr.or.ddit.http;
 
-import java.io.BufferedInputStream;
+//http://localhost:8888/efg/index
+//
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
+import java.net.URLConnection;
+import java.net.URLDecoder;
 import java.util.StringTokenizer;
 
 /**
- * 
  * 간단한 웹서버 예제
- *
  */
 public class MyHTTPServer {
 	private final int port = 8888;
@@ -22,12 +24,10 @@ public class MyHTTPServer {
 
 	// 서버 시작
 	public void start() {
-		
-		System.out.println("HTTP 서버가 시작되었습니다");
 
-		try (ServerSocket server = 
-				new ServerSocket(this.port)) {
-			
+		System.out.println("HTTP 서버가 시작되었습니다.");
+
+		try (ServerSocket server = new ServerSocket(this.port)) {
 			while (true) {
 				try {
 					Socket socket = server.accept();
@@ -40,12 +40,14 @@ public class MyHTTPServer {
 					ex.printStackTrace();
 				}
 			}
-
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 	}
 
+	/**
+	 * Http 요청 처리를 위한 스레드 클래스
+	 */
 	private class HttpHander extends Thread {
 
 		private final Socket socket;
@@ -60,49 +62,76 @@ public class MyHTTPServer {
 			BufferedReader br = null;
 
 			try {
-				out = new BufferedOutputStream(
-						socket.getOutputStream());
-				br = new BufferedReader(
-						new InputStreamReader(
-								socket.getInputStream()));
-				
-				//요청헤더 정보 파싱하기
+				out = new BufferedOutputStream(socket.getOutputStream());
+				br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				// 요청헤더 정보 파싱하기
+
 				StringBuilder sb = new StringBuilder();
-				
-				//Request Line(첫줄은 요청라인)
+
+				// Request Line(첫줄은 요청라인)
 				String reqLine = br.readLine();
-				printMsg("Request Line: ",reqLine);
-				
-				while(true) {
+
+				printMsg("Request Line : ", reqLine);
+
+				while (true) {
 					String str = br.readLine();
-					
-					//Empty Line 체크
-					if(str.equals("")) break;
-					sb.append(str+"\n");
+
+					// Empty Line 체크
+					if (str.equals(""))
+						break;
+
+					// 속도 빨라짐
+					sb.append(str + "\n");
 				}
-						
-				//헤더정보(Header)
+
+				// 헤더 정보(Header)
+				// 문자열로 리턴
 				String reqHeaderStr = sb.toString();
-				
-				printMsg("요청헤더: " , reqHeaderStr);
-				
-				String reqPath = ""; //요청경로
-				
-				//요청 페이지 정보 가져오기
-				StringTokenizer st = 
-						new StringTokenizer(reqLine);
-				
-				while(st.hasMoreTokens()) {
+
+				printMsg("요청헤더 : ", reqHeaderStr);
+
+				String reqPath = ""; // 요청경로
+
+				// 요청 페이지 정보 가져오기
+				StringTokenizer st = new StringTokenizer(reqLine);
+				while (st.hasMoreTokens()) {
 					String token = st.nextToken();
-					if(token.startsWith("/")) {
+					if (token.startsWith("/")) {
 						reqPath = token;
 						break;
 					}
 				}
-				
-			System.out.println("reqPath => " + reqPath);	
-						
+				System.out.println("reqPath => " + reqPath);
 
+				// URL 디코딩 처리(한글깨짐 문제 해결)
+				reqPath = URLDecoder.decode(reqPath, encoding);
+
+				String filePath = "./WebContent" + reqPath;
+
+				// 해당 파일 이름을 이용하여 Content=type 정보 추출하기
+				String contentType = URLConnection.getFileNameMap().getContentTypeFor(filePath);
+
+				// CSS파일인 경우 인식이 안되사 추가함.
+				if (contentType == null && File.pathSeparator.endsWith(".css")) {
+					contentType = "text/css";
+				}
+
+				File file = new File(filePath);
+				if (!file.exists()) {
+					makeErrorPage(out, 404, "Not Found");
+					return;
+				}
+
+				byte[] body = makeResponseBody(filePath);
+				byte[] header = makeResponseHeader(body.length, contentType);
+
+				// 응답한 헤더 데이터 보내기
+				out.write(header);
+
+				// 응답내용 보내기 전 반드시 Empty Line을 보내야한다.
+				out.write(body);
+				out.flush();
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			} finally {
@@ -113,17 +142,85 @@ public class MyHTTPServer {
 				}
 			}
 		}
-		
+
 		private void printMsg(String title, String msg) {
-			System.out.println("============================");
-			System.out.println("titel");
-			System.out.println("============================");
-			System.out.println("msg");
-			System.out.println("----------------------------");
+			System.out.println("========================================");
+			System.out.println("title");
+			System.out.println("========================================");
+			System.out.println(msg);
+			System.out.println("----------------------------------------");
+		}
+	}
+
+	/**
+	 * 응답헤더 생성하기
+	 * 
+	 * @param length      응답내용 크기
+	 * @param contentType MINE타입
+	 * @return 헤더정보 바이트 배열
+	 */
+	private byte[] makeResponseHeader(int length, String contentType) {
+		String header = "HTTP/1.1 200 OK\r\n" + "Server: My HTTPServer 1.0\r\n" + "Content-length: " + length + "\r\n"
+				+ "Content-type: " + contentType + "; charset=" + this.encoding;
+
+		return header.getBytes();
+	}
+
+	/**
+	 * 여러페이지 생성
+	 * 
+	 * @param out        출력용 스트림
+	 * @param statusCode 상태코그
+	 * @param errMsg     에러메시지
+	 */
+	private void makeErrorPage(
+			OutputStream out, int statusCode, String errMsg) {
+
+		String statusLine = "HTTP/1.1  " + " " 
+					+ statusCode + " " + errMsg;
+
+		try {
+			out.write(statusLine.getBytes());
+			out.flush();
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+
 		}
 
 	}
-	
+
+	/**
+	 * 응답내용 생성하기
+	 * 
+	 * @param filePath 응답으로 사용할 파일경로
+	 * @return 바이트배열 데이터
+	 */
+	private byte[] makeResponseBody(String filePath) {
+
+		FileInputStream fis = null;
+		byte[] data = null;
+
+		try {
+			File file = new File(filePath);
+			data = new byte[(int) file.length()];
+
+			fis = new FileInputStream(file);
+			fis.read(data);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return data;
+	}
+
 	public static void main(String[] args) {
 		new MyHTTPServer().start();
 	}
---
+++ JavaXMLTest/src/kr/or/ddit/basic/T02DOMParsingTest.java
@@ -0,0 +1,78 @@
+package kr.or.ddit.basic;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class T02DOMParsingTest {
+	public void parse() throws ParserConfigurationException, SAXException, Exception {
+		// XML문서를 생성하기 위한 DocumentBuilder객체 생성하기
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = dbf.newDocumentBuilder();
+
+		// Document 객체 생성
+		Document document = 
+				builder.parse(new File("./src/new_book.xml"));
+
+		// DOM Document 객체로부터 루트엘리먼트 객체 가져오기
+		Element root = document.getDocumentElement();
+		System.out.println("루트엘리먼트 태그명 " + root.getTagName());
+		
+		//하위 엘리먼트 접근방법1 : getElememtsByTagName()이용
+		NodeList bookNodeList = 
+				root.getElementsByTagName("book");
+		Node firstBookNode = bookNodeList.item(0);//첫번째 항목
+		Element firstBookElement = (Element) firstBookNode;
+		
+		//속성접근방법 1 : 엘리먼트 객체의 getAttribute()이용
+		System.out.println("엘리먼트 객체의 getAttribute() 이용 =>"
+				+ firstBookElement.getAttribute("isbn"));
+		
+		//속성접근방법2: 노드 객체의 getAttribute()이용
+		NamedNodeMap nodeMap = firstBookNode.getAttributes();
+		System.out.println("노드 객체의  getAttribute() 이용 => " 
+					+ nodeMap.getNamedItem("isbn").getNodeValue());
+		
+		//하위 엘리먼트 접근방법2 : getChildNodse()이용
+		NodeList firstBookChildNodeList = 
+				firstBookNode.getChildNodes();
+		Node titleNode = firstBookChildNodeList.item(1);
+		Element titleElement = (Element) titleNode;
+		System.out.println("titleElement.getTagName() => " 
+				+ titleElement.getTagName());
+		System.out.println("titleElement.getContent() => " 
+				+ titleElement.getTextContent());
+		
+		//전체 출력하기
+		System.out.println("----------------------------------------");
+		System.out.printf("%8s %8s %15s %10s %8s\n ",
+				"ISBN" ,"분류" ,"제목" ,"저자" ,"가격");
+		System.out.println("----------------------------------------");
+		for(int i =0; i<bookNodeList.getLength(); i++) {
+			Element element = (Element) bookNodeList.item(i);
+			String isbn = element.getAttribute("isbn");
+			String kind = element.getAttribute("kind");
+			String title = element.getElementsByTagName("title")
+					.item(0).getTextContent();
+			String author = element.getElementsByTagName("author")
+					.item(0).getTextContent();
+			String price = element.getElementsByTagName("price")
+					.item(0).getTextContent();
+			System.out.printf("%8s %8s %15s %10s %8s\n ",
+					isbn,kind,title,author,price);
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		new T02DOMParsingTest().parse();
+	}
+}
